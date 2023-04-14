3eb490928fdab24abdc1bdaaae7f80ed38826933
DevNalther <nalther.l.b.ganem@gmail.com>
2023-04-14 12:37:24 -0300

    finalizado

--- 1 - fundamentos/secao-03-introducao-javascript/dia1-intro-JV/script.js
+++ 1 - fundamentos/secao-03-introducao-javascript/dia1-intro-JV/script.js
@@ -68,14 +68,92 @@
 // };
 
 
-let nota = 50;
+// let nota = 50;
 
-if(nota >= 80) {
-    console.log('Parabéns, você faz parte do grupo de pessoas aprovadas!');
-}
-else if(nota < 80 && nota >= 60) {
-    console.log('Você está na lista de espera.');
-}
-else {
-    console.log('Infelizmente, você reprovou.');
-}
+// if(nota >= 80) {
+//     console.log('Parabéns, você faz parte do grupo de pessoas aprovadas!');
+// }
+// else if(nota < 80 && nota >= 60) {
+//     console.log('Você está na lista de espera.');
+// }
+// else {
+//     console.log('Infelizmente, você reprovou.');
+// }
+
+// '||' OR
+// '&&' AND
+// '!' NOT
+
+// let comida = 'pão na chapa';
+// let bebida = 'café';
+
+// if (bebida === 'café' && comida === 'pão na chapa') {
+//   console.log('Muito obrigado(a) pela refeição :)');
+// } else {
+//   console.log('Acho que houve um engano com meu pedido');
+// };
+
+
+// let currentHour = 14.22;
+// let message = ' ';
+
+// if(currentHour >= 22) {
+//     message = 'Não deveríamos comer nada, é hora de dormir'
+//     console.log(message);
+// }
+// else if(currentHour >= 18 && currentHour < 22) {
+//     message = 'Rango da noite, vamos jantar :D'
+//     console.log(message);
+// }
+// else if(currentHour >= 14 && currentHour < 18) {
+//     message = 'Vamos fazer um bolo pro café da tarde?'
+//     console.log(message);
+// }
+// else if(currentHour >= 11 && currentHour < 14) {
+//     message = 'Hora do almoço!!!'
+//     console.log(message);
+// }
+// else if(currentHour >= 4 && currentHour <= 11) {
+//     message = 'Hmmm, cheiro de café recém-passado'
+//     console.log(message);
+// }
+
+// let bebida1 = 'suco';
+// let bebida2 = 'cafe';
+
+// if(bebida1 === 'espumante' || bebida2 === 'ice') {
+//     console.log('Obrigado, tenha um bom dia');
+// }
+// else {
+//     console.log('Eu não pedi isso.');
+// };
+
+
+// let weekDay = 'sabado';
+// if(weekDay === 'segunda-feira' || weekDay === 'terça-feira' || weekDay === 'quarta-feira' || weekDay === 'quinta-feira' || weekDay === 'sexta-feira') {
+//     console.log('Mais de um dia de Estudo na TRYBE!');
+// } else {
+//     console.log('FINALMENTE!!!');
+// }
+
+// let sts = 'null';
+
+// switch(sts) {
+//     case 'aprovada':
+//         console.log('Parabéns, você está no grupo de pessoas aprovadas!');
+//         break;
+
+//     case 'lista':
+//         console.log('Você está na nossa lista de espera');
+//         break;
+    
+//     case 'reprovada':
+//         console.log('Infelizmente, você reprovou.');
+//         break;
+        
+//     default:
+//         console.log('informação incorreta');
+// }
+
+
+
