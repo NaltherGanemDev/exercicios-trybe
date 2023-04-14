45788302ddb1ae440536a559dc4a825e33e249d6
DevNalther <nalther.l.b.ganem@gmail.com>
2023-04-14 09:10:28 -0300

    vqv

---
+++ 1 - fundamentos/secao-03-introducao-javascript/dia1-intro-JV/script.js
@@ -0,0 +1,81 @@
+// let myName = 'Nalther';
+// let birthCity = 'Salvador';
+// let birthYear = 2001;
+
+
+// console.log('Meu nome é ' + myName + '. Nasci em ' + birthCity + ', no ano de ' +birthYear + '.');
+
+
+
+// let sal = 2550;
+
+// console.log(sal + sal);
+// console.log(sal - sal);
+// console.log(sal * sal);
+// console.log(sal / sal);
+// console.log(sal ** sal);
+
+// '++' ou '--' ou '+=' ou '-=' incrementa ou decrementa uma unidade ao valor da variavel 
+
+// sal++;
+// sal--;
+
+// sal -= 1;
+
+// console.log(sal);
+
+
+
+// let patientId = 50;  //number
+// let isEnrolled = true; //boleano
+// let patientInfo = { //objeto
+//   firstName: 'Ana', //string
+//   lastName: 'Santos', //string
+// };
+// let patientEmail = 'ana@email.com'; //string
+
+// console.log(typeof patientId);
+// console.log(typeof isEnrolled);
+// console.log(typeof patientInfo);
+// console.log(typeof patientEmail);
+// console.log(typeof patientAge);
+
+
+
+// let base = 5;
+// let height = 8;
+// let area = (base * height);
+// let perimeter = ((base * 2) + (height * 2))
+
+// console.log(area);
+// console.log(perimeter);
+
+
+
+// let trybe = 18.30;
+
+// if (trybe >= 14 && trybe < 14.40) {
+//     console.log('Momento Inicial');
+// } 
+// else if (trybe >= 16.20 && trybe < 18) {
+//     console.log('Aula Ao Vivo');
+// } 
+// else if (trybe >= 19.30 && trybe < 20) {
+//     console.log('Fechamento');
+// } 
+// else {
+//     console.log('VÁ ESTUDAR');
+// };
+
+
+let nota = 50;
+
+if(nota >= 80) {
+    console.log('Parabéns, você faz parte do grupo de pessoas aprovadas!');
+}
+else if(nota < 80 && nota >= 60) {
+    console.log('Você está na lista de espera.');
+}
+else {
+    console.log('Infelizmente, você reprovou.');
+}
